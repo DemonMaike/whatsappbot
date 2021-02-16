e67b9af3362007fa94247795a16b35bb1393f1ae
DemonMaike <dmitry.malyshevv@yandex.ru>
2021-02-16 22:01:32 +0300

    working bot

---
+++ DB.py
@@ -0,0 +1,35 @@
+import sqlite3
+# Create con and cur for job's be database  
+#conn = sqlite3.connect('database.db')
+#cur = conn.cursor()
+
+# select name birthday man's 
+
+def name(dt):
+    '''This function return name birthday man's, need write a date'''
+    conn = sqlite3.connect('database.db')
+    cur = conn.cursor()
+    end = []
+    result = [x for x in cur.execute('SELECT Name FROM Human WHERE date = "{}"'.format(dt))]
+    for x in result:
+        end.append(x[0])
+
+    return ', '.join(end)
+
+
+
+
+
+# create table
+# cur.execute("""CREATE TABLE Human (Name text, date text)""")
+
+# Write information of humans
+# Human = [('Первый', '2, 13'), ('Второй', '2, 13'), ('Третий', '2, 13')]
+
+# delete information
+# cur.execute("DELETE FROM Human WHERE Date = '2 13'" )
+# con.commit()
+
+# Update data from database
+#cur.execute('UPDATE Human SET date = "2, 15" WHERE date = "2, 13" ')
+#conn.commit()
--- main.py
+++ main.py
@@ -1,30 +1,17 @@
-import requests 
+import requests
+import DB
 from datetime import datetime
+import secret
 
-# detect for date today
+
+# detected the date today
 d = datetime.today()
 dt = f'{d.month}, {d.day}'
 
-# testing DB
-human = {
-     "2, 1": "Первый",
-     "2, 2": "Второй",
-     "2, 3": "Третий"
-         }
+data = {"chatid":secret.idchat, "message":f"{DB.name(dt)} c Днем рождения!"}
 
-# logic the bot, if today date is birthday user's, then write name in message. im stupid
-name = []
-for date in human.keys():
-    if dt == date:
-        name.append(human[dt])
-
-
-# Setting for bot message, chat id and text
-    data = {
-        "chatid":"79162149060@c.us",
-        "message":f"{name[0]}, c днем рождения!"
-    }
-
-#request for What's app API
-request = requests.post("https://api.green-api.com/waInstance7896/SendMessage/72aa39e3263a85b1d0125c7bde800136b63bfaa1c38ebb4135", json=data)
+#request for What's app API, url from green-api
+request = requests.post(secret.apiurl, json=data)
 print(request)
+print(secret.apiurl)
+
