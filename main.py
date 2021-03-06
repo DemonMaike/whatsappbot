2d215035038e0a1f1e46c21e49c47f7241d882b9
DemonMaike <dmitry.malyshevv@yandex.ru>
2021-03-07 00:56:36 +0300

    heroku setup

--- main.py
+++ main.py
@@ -3,14 +3,20 @@ import DB
 from datetime import datetime
 import secret
 
+while True:
 
-# detected the date today
-d = datetime.today()
-dt = f'{d.month}, {d.day}'
+    # detected the date today
+    d = datetime.today()
+    time = datetime.now().time()
+    print(str(time)[0:8])
+    if str(time)[0:8] == '12:25:00':
+        dt = f'{d.month}, {d.day}'
 
-data = {"chatid":secret.idchat, "message":f"{DB.name(dt)} c Днем рождения!"}
+        data = {"chatid":secret.idchat, "message":f"{DB.name(dt)} c Днем рождения!"}
 
-#request for What's app API, url from green-api
-request = requests.post(secret.apiurl, json=data)
-print(request)
-print(secret.apiurl)
+        #request for What's app API, url from green-api
+        request = requests.post(secret.apiurl, json=data)
+        print(request)
+        print(secret.apiurl)
+        time.sleep(3)
+    
