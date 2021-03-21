23b0b6b2a9a99a012e8ab40743b59e6fe90472af
DemonMaike <dmitry.malyshevv@yandex.ru>
2021-03-21 13:51:54 +0300

    new bot

--- secret.py
+++ secret.py
@@ -1,2 +1,3 @@
-idchat = "79154378196-1613418125@g.us"
+idchat = "79998074797-1616322210@g.us"
 apiurl = "https://api.green-api.com/waInstance7447/SendMessage/30aba6731fde660c6610b8a7556dbdb73f403803edadbdff51"
+
