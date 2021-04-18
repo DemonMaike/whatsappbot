022dd904ab503c5d64db09d12087a1f74ebf394b
DemonMaike <dmitry.malyshevv@yandex.ru>
2021-04-18 20:18:19 +0300

    part 1

---
+++ data.py
@@ -0,0 +1,12 @@
+
+import psycopg2
+
+con = psycopg2.connect(
+    database="dbr0eanbod0c9q",
+    user="tvoflxtnevefnc",
+    password="",
+    host="127.0.0.1",
+    port="5432"
+)
+
+print("Database opened successfully")
