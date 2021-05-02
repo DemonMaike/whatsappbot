4c2e7c7e810aaeb4da8ae6efd44aed262a3d170b
Serkoes <timoha-john@yandex.ru>
2021-05-02 12:57:28 +0300

    add test point

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ while True:
     time = now.strftime('%H:%M')
     date_now = now.strftime('%m-%d')
     # writeing a congretulation message in what's app in need time
-    if time == '05:50' or os.getenv("TEST") == "TEST": 
+    if time == '5:55' or os.getenv("TEST") == "TEST": 
         data = {"chatid":secret.idchat, "message":f"{name(date_now)} c Днем рождения!"}
         print("Is worked" , data)
         #request for What's app API, url make from green-api
